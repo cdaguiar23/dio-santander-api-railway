2c62ecfb2aafb3f47a172a66f781d7af76e489af
Claudionei de Aguiar <caguiar23@gmail.com>
2024-09-16 00:14:28 -0300

    Criçaõ do projeto inicial

---
+++ src/main/java/me/dio/SatanderDevWeekApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatanderDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatanderDevWeekApplication.class, args);
+	}
+
+}
